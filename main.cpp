97bf26c339271a2e5a150540a912483af1dc2594
Jary Liu <49688036+git-flyer@users.noreply.github.com>
2023-03-26 10:34:30 +0800

    renew test example

--- main.cpp
+++ main.cpp
@@ -6,32 +6,32 @@
 using namespace std;
 
 int main() {
-    RB_Tree<int> r = RB_Tree<int>(1);
-    for (int i = 2; i <= 10; ++i) {
-        r.Insert(i);
-    }
-    r.Delete(6);
-    queue<RB_Tree_Node<int> *> q;
-    RB_Tree_Node<int> *tmp = r.root;
-    q.push(tmp);
-    //简陋的层序遍历
-    while (!q.empty()) {
-        int n = q.size();
-        for (int i = 0; i < n; ++i) {
-            RB_Tree_Node<int> *p = q.front();
-            cout << p->data << " " << p->color<<"   ";
-            q.pop();
-            if (p->left)
-                q.push(p->left);
-            if (p->right)
-                q.push(p->right);
-        }
-        cout << "\n";
-    }
-    system("pause");
-    return 0;
+    //RB_Tree<int> r = RB_Tree<int>(1);
+    //for (int i = 2; i <= 10; ++i) {
+    //    r.Insert(i);
+    //}
+    //r.Delete(6);
+    //queue<RB_Tree_Node<int> *> q;
+    //RB_Tree_Node<int> *tmp = r.root;
+    //q.push(tmp);
+    ////简陋的层序遍历
+    //while (!q.empty()) {
+    //    int n = q.size();
+    //    for (int i = 0; i < n; ++i) {
+    //        RB_Tree_Node<int> *p = q.front();
+    //        cout << p->data << " " << p->color<<"   ";
+    //        q.pop();
+    //        if (p->left)
+    //            q.push(p->left);
+    //        if (p->right)
+    //            q.push(p->right);
+    //    }
+    //    cout << "\n";
+    //}
+    //system("pause");
     HashTable<int> h = *(new HashTable<int>(10));
-    for (int i = 1; i < 100; ++i)
+    for (int i = 1; i < 10; ++i)
         h.Insert(i);
+    h.Delete(8);
     return 0;
 }
